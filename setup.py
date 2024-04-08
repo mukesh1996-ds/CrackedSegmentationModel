e6ec3e0d0e0f8493f3bafe887d09d371d5046ba7
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-04-08 11:10:19 +0530

    template created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Cracked Detection',
+    version= '0.0.0',
+    author= 'Mukesh',
+    author_email= 'mks.mukesh1996@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
