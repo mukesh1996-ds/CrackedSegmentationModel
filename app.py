3a93b19a48f60777ac153b43fa61c545fa53997e
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-04-13 22:58:46 +0530

    utils,exception added

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from Cracked_Detection.logger import logging
+from Cracked_Detection.exception import AppException
+import sys
 
-logging.info("Welcome to Custom logs")
+
+try:
+
+    a = 3/"small"
+except Exception as e:
+    raise AppException(e,sys)
+
 
 
 
