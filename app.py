3e2a4d3ffd3f70fa9f7053e0e753aa3befe0b85a
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-04-14 14:05:29 +0530

    Completed

--- app.py
+++ app.py
@@ -1,4 +1,59 @@
-from Cracked_Detection.pipeline.training_pipeline import TrainPipeline
+from flask import Flask, request, jsonify, render_template
+from ultralytics import YOLO  
+import base64
+import os
+import shutil
 
-obj = TrainPipeline()
-obj.run_pipeline()
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+def decodeImage(encoded_image, filename):
+    with open(filename, "wb") as fh:
+        fh.write(base64.b64decode(encoded_image))
+
+def get_latest_predict_folder():
+    predict_folders = [folder for folder in os.listdir('runs/segment') if folder.startswith('predict')]
+    sorted_folders = sorted(predict_folders, reverse=True)
+    if sorted_folders:
+        latest_folder = sorted_folders[0]
+        return os.path.join('runs/segment', latest_folder)
+    else:
+        return None
+
+def move_and_encode_latest_image_into_base64():
+    latest_predict_folder = get_latest_predict_folder()
+    
+    if latest_predict_folder:
+        input_image_path = os.path.join(latest_predict_folder, 'inputImage.jpg')
+        destination_folder = "runs/final_prediction"
+        shutil.move(input_image_path, destination_folder)
+        with open(os.path.join(destination_folder, 'inputImage.jpg'), "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        os.remove(os.path.join(destination_folder, 'inputImage.jpg'))
+        return encoded_string
+    else:
+        return None
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.json:
+        return jsonify({'error': 'No image found in request'}), 400
+
+    image = request.json['image']
+    filename = "data/inputImage.jpg"
+    decodeImage(image, filename)
+
+    model = YOLO('runs/segment/train2/weights/best.pt')
+    results = model.predict(source=filename, save=True)
+
+    opencodedbase64 = move_and_encode_latest_image_into_base64()
+
+    result = {"image": opencodedbase64.decode('utf-8')}
+
+    return jsonify(result), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
