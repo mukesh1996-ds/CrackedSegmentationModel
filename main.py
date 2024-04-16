57265dcc2ac08bf7624720adcfafd07e969fd9cc
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-04-16 09:59:16 +0530

    deployed successfully

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Cracked_Detection.pipeline.training_pipeline import TrainPipeline
 from Cracked_Detection.utils.main_utils import decodeImage, encodeImageIntoBase64
 from ultralytics import YOLO  
 import base64
-import os
+import os,glob,time,re
 import shutil
 from flask_cors import CORS, cross_origin
 
@@ -39,10 +39,21 @@ def predict():
 
     # Run YOLO prediction command
     model = YOLO('runs/segment/train2/weights/best.pt')
+    # Predict using YOLO
     results = model.predict(source=filename,save=True)
-    print(results)
+
+    parent_directory = "runs/segment/"
+    directories = [d for d in os.listdir(parent_directory) if os.path.isdir(os.path.join(parent_directory, d))]
+    print(directories)
+    pattern = "predict"
+    filtered_directories = [d for d in directories if d.startswith(pattern) and d[len(pattern):].isdigit()]
+    latest_directory = max(filtered_directories, key=lambda x: int(re.search(r'\d+$', x).group()))
+    print(latest_directory)
+    image_path = os.path.join(parent_directory, latest_directory, "inputImage.jpg").replace('\\', '/')
+
     # Encode the resulting image into base64
-    opencodedbase64 = encodeImageIntoBase64(r"runs/segment/predict/inputImage.jpg")
+    opencodedbase64 = encodeImageIntoBase64(image_path)
+
     result = {"image": opencodedbase64.decode('utf-8')}
     return jsonify(result), 200
 
